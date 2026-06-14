f6923c8fcf3898b0ff543d47ff45498d2e2c0a25
denz90 <denzelobengkumi29@gmail.com>
2026-06-14 16:05:02 +0000

    feat: allow all CORS origins and add environment configuration files

--- backend/main.py
+++ backend/main.py
@@ -27,8 +27,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["Content-Type", "Authorization"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include routers
